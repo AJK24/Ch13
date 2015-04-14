4ceaadefca844b7779005c5192cc27213551d850
Andrew <akv9509@saintjoe.edu>
2015-04-14 15:34:22 -0400

    tt

--- seqlast.js
+++ seqlast.js
@@ -9,6 +9,7 @@ function swap(arr, index, index1) {
 var numbers = [5,1,7,4,2,10,9,3,6,2,8];
 print(numbers);
 for (var i = 1; i <= 3; i++) {
-		seqSearch(numbers, 7777777);
+		seqSearch(numbers, 8);
+		swap(numbers, 0, i);
 			print(numbers);
 }
